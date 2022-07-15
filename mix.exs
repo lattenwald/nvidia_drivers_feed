699e941d6e7db9bc487fe75f90a1d1f408908192
Alexander Q <qalexx@gmail.com>
2022-07-15 13:48:31 +0300

    stuff

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule NvidiaDriversFeed.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
 
@@ -32,4 +33,15 @@ defmodule NvidiaDriversFeed.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp releases do
+    [
+      docker: [
+        include_executables_for: [:unix],
+        steps: [:assemble, :tar],
+        path: "/app/release"
+      ]
+    ]
+  end
+
 end
